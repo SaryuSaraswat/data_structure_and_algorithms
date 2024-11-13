f78f9dd319f74718a02d0274c7aae3e0ee0b35fc
Saryu Saraswat <21cse107@gweca.ac.in>
2024-11-13 10:52:18 +0530

    added code for add node in middle and print linkedist

--- 06LinearDataStructure/LinkedList/LinkedList.java
+++ 06LinearDataStructure/LinkedList/LinkedList.java
@@ -21,6 +21,7 @@ public class LinkedList{
         Node newNode = new Node(data);
         if(head == null){
             head = tail = newNode;
+            return;
         }
 
         //step 2 : newNode's next
@@ -35,17 +36,58 @@ public class LinkedList{
         Node newNode = new Node(data);
         if(head == null){
             head = tail = newNode;
+            return;
         }
 
         tail.next = newNode;
         tail = newNode;
     }
 
+    public static void print(){
+        
+        Node temp = head; //we assign head to a temp variable
+       
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+
+    }
+
+    public void add(int idx, int data){
+
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while(i < idx-1){
+            temp = temp.next;
+            i++;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
     public static void main(String [] args){
         LinkedList ll = new LinkedList();
+        print();
         ll.addFirst(2);
+        print();
         ll.addFirst(1);
+        print();
         ll.addLast(3);
+        print();
         ll.addLast(4);
+        print();
+
+        ll.add(2, 9);
+        print();
     }
 }
